0be08b62371323b07c981a4285d2026578ed29d2
mrsergazinov <mrsergazinov>
2022-04-23 15:34:44 -0500

    Added forward method wraper

--- model_tree.py
+++ model_tree.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from model_node import *
+from model import AutoEncoder
 
 class iSHM_block(nn.Module):
     def __init__(self, input_dim, cp_k_max) -> None:
@@ -20,23 +20,28 @@ class iSHM_block(nn.Module):
         return torch.clamp(p, min=1e-4, max=1-1e-4)
 
 class TreeNVAE(nn.Module):
-    def __init__(self, depth, cp_k_max, feat_dim, use_cuda, **kwargs) -> None:
+    def __init__(self, depth, cp_k_max, feat_dim, **kwargs):
         super(TreeNVAE, self).__init__()
         self.depth = depth
         self.cp_k_max = cp_k_max
         self.feat_dim = feat_dim
-        self.use_cuda = use_cuda
 
-        if depth == 0:
-            self.child_l = NVAE(**kwargs)
-            self.child_r = NVAE(**kwargs)
+        if self.depth == 1:
+            self.child_l = AutoEncoder(**kwargs)
+            self.child_r = AutoEncoder(**kwargs)
         else:
-            self.child_l = TreeNVAE(depth - 1, cp_k_max, use_cuda, **kwargs)
-            self.child_r = TreeNVAE(depth - 1, cp_k_max, use_cuda, **kwargs)
+            self.child_l = TreeNVAE(depth - 1, cp_k_max, **kwargs)
+            self.child_r = TreeNVAE(depth - 1, cp_k_max, **kwargs)
         self.ishm_block = iSHM_block(feat_dim, cp_k_max)
         
-    def forward(self, x, p_x_previous) -> torch.Tensor:
+    def forward(self, x, p_x_previous=None, **kwargs):
         p_r = self.ishm_block(x) 
         if p_x_previous is None:
             p_x_previous = torch.ones_like(p_r)
-        return self.child_r(x, p_x_previous * p_r) + self.child_l(x, p_x_previous * (1.0 - p_r))
+
+        if self.depth == 1:
+            out_l = self.child_l(x, p_x_previous * (1.0 - p_r), **kwargs)
+            out_r = self.child_r(x, p_x_previous * p_r, **kwargs)
+            return out_l[-1] + out_r[-1]
+        else:
+            return self.child_r(x, p_x_previous * p_r, **kwargs) + self.child_l(x, p_x_previous **kwargs)
